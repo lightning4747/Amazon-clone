def1d8be98d9b8832056d089e89bf022006bc5b3
lightning4747 <vignesh112847@gmail.com>
2025-12-20 12:52:15 +0530

    create automated tests for formatcurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,29 @@
+import { formatcurrency } from "../scritps/utils/money.js";
+
+console.log("test suit: format currency");
+
+if(formatcurrency(2095) === '20.95') {
+    console.log("passed");
+}
+else {
+    console.log('failed');
+}
+if(formatcurrency(0) === '0.00') {
+    console.log("passed");
+}
+else {
+    console.log('failed');
+}
+if(formatcurrency(136789657024580) === '1367896570245.80') {
+    console.log("passed");
+}
+else {
+    console.log('failed');
+}
+
+if(formatcurrency(2000.5) === '20.01') {
+    console.log("passed");
+}
+else {
+    console.log('failed');
+}
