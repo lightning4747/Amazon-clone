6aabd598fac9907bc86a9d2f38b4eb616958bd2c
lightning4747 <vignesh112847@gmail.com>
2025-12-21 19:19:08 +0530

    setup the backend for learning.

--- scritps/checkout.js
+++ scritps/checkout.js
@@ -1,6 +1,7 @@
 import { orderSummary } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 import '../data/classCart.js';
+import '../data/backend.js';
 
 renderPaymentSummary();
 orderSummary();
